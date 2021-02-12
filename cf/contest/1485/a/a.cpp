7845aa16aedc53192b841be37b652f9dd95b682b
Humayan-Kabir <zyxwv.hk@gmail.com>
2021-02-12 23:45:59 +0600

    cf 1485 added

---
+++ cf/contest/1485/a/a.cpp
@@ -0,0 +1,55 @@
+/* بسم الله الرحمن الرحيم
+ * Author: kabir10
+ * Time: 2021-02-12 20:50:41
+**/
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+        int ans = INT_MAX;
+        for(int i = b > 1 ? 0 : 1; i <= min(32, a); i++) {
+            int tA = a, tB = b + i, result = i;
+            while(tA > 0) {
+                result++;
+                tA /=  tB;
+            }
+            ans = min(ans, result);
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
